437904ad9e297d60ad39c170415c7bcf152df927
Margarita <ricunsos@gmail.com>
2019-10-15 10:47:41 +0300

    interface

--- BezierCubicSplines/BezierCubicSplines/Form1.Designer.cs
+++ BezierCubicSplines/BezierCubicSplines/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(736, 615);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(773, 563);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(164, 64);
+            this.ClearBtn.TabIndex = 1;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(961, 655);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "BezierCurve";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ClearBtn;
     }
 }
 
--- BezierCubicSplines/BezierCubicSplines/Form1.cs
+++ BezierCubicSplines/BezierCubicSplines/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,9 +12,87 @@ namespace BezierCubicSplines
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        private SolidBrush brushRed = new SolidBrush(Color.Red);
+        private SolidBrush brushBlack = new SolidBrush(Color.Black);
+        private Pen penHull = new Pen(Color.LightGoldenrodYellow, 1);
+        private Pen penCurve = new Pen(Color.Magenta, 1);
+        private List<PointF> points = new List<PointF>();
+        PointF currentPoint = new PointF(float.NaN, float.NaN);
+        PointF nap = new PointF(float.NaN, float.NaN);
+        int ind = -1;
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.Clear(Color.White);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                PointF h = e.Location;
+                if (IsInRadius(ref h) && ind != (-1))
+                    points.Remove(points[ind]);
+                currentPoint = e.Location;
+                g.Clear(Color.White);
+            }
+            pictureBox1.Invalidate();
+        }
+
+        //попадает ли выбранная точка в радиус одной из уже добавленных
+        bool IsInRadius(ref PointF e)
+        {
+            PointF p = new PointF(0, 0);
+            for(int i = 0;i < points.Count;i++)
+            {
+                p = points[i];
+                if (p.X - 15 <= e.X && e.X <= p.X + 15 && p.Y - 15 <= e.Y && e.Y <= p.X + 15)
+                {
+                    ind = i;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                currentPoint = e.Location;
+            pictureBox1.Invalidate();
+        }
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                currentPoint = e.Location;
+                points.Add(currentPoint);
+                currentPoint = nap;
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (PointF p in points)
+            {
+                e.Graphics.FillEllipse(brushRed, p.X - 3, p.Y - 3, 7, 7);
+            }
+            e.Graphics.FillEllipse(brushBlack, currentPoint.X - 3, currentPoint.Y - 3, 7, 7);
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            points.Clear();
+            pictureBox1.Invalidate();
         }
     }
 }
